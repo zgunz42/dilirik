b63e6a5eecf8c1b057767c4c3229f0a995a5fc6e
I Kadek Adi Gunawan <adi_gunawan@live.com>
2019-08-03 11:47:37 +0800

    intent move from main to about activity

--- app/src/main/java/com/kangmicin/dilirik/MainActivity.kt
+++ app/src/main/java/com/kangmicin/dilirik/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.kangmicin.dilirik
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +19,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            R.id.item_about -> {
+                openAboutPage()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun openAboutPage() {
+        var openPage = Intent(this@MainActivity, AboutActivity::class.java)
+        startActivity(openPage)
     }
 
     fun toastText(view: android.view.View) {
